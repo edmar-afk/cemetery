89c5407bc516757d9812223eb04ea3f4b7c3c67d
project2479 <jaywrsnp6@gmail.com>
2024-07-29 11:32:01 +0800

    new UI

---
+++ src/components/Caretaker/PlotModal.jsx
@@ -0,0 +1,50 @@
+import * as React from "react";import Button from "@mui/joy/Button";
+import Modal from "@mui/joy/Modal";
+import ModalDialog from "@mui/joy/ModalDialog";
+import DialogTitle from "@mui/joy/DialogTitle";
+import DialogContent from "@mui/joy/DialogContent";
+import Stack from "@mui/joy/Stack";
+import { TextField } from "@mui/material";
+import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
+// eslint-disable-next-line react/prop-types
+export default function PlotModal({ modalIsOpen, handleClose }) {
+	return (
+		<React.Fragment>
+			<Modal
+				open={modalIsOpen}
+				onClose={handleClose}>
+				<ModalDialog>
+					<DialogTitle>Available Plots in Center Cemetery</DialogTitle>
+					<DialogContent>Fill in the number of available Plots.</DialogContent>
+					<form
+						onSubmit={(event) => {
+							event.preventDefault();
+							handleClose(); // Close the modal on form submission
+						}}>
+						<Stack spacing={2}>
+							<TextField
+								id="number"
+								className="number"
+								label="Number of Available Plots"
+								variant="outlined"
+								size="small"
+								sx={{
+									margin: "10px auto",
+									"& .MuiOutlinedInput-root": {},
+								}}
+							/>
+							
+							<Button type="submit">
+								<AddCircleOutlinedIcon
+									fontSize="small"
+									className="mr-1"
+								/>{" "}
+								Add
+							</Button>
+						</Stack>
+					</form>
+				</ModalDialog>
+			</Modal>
+		</React.Fragment>
+	);
+}
--- src/routes/CaretakerDashboard.jsx
+++ src/routes/CaretakerDashboard.jsx
@@ -1,26 +1,33 @@
-import { useState } from "react";import NavBar from "../components/Caretaker/NavBar";
-import Bubbles from "../components/Caretaker/Bubbles";
+import { useState } from "react";import NavBar from "../components/Caretaker/NavBar";import Bubbles from "../components/Caretaker/Bubbles";
 import Time from "../components/Caretaker/Time";
 import Kalag from "../components/dashboard/Kalag";
 import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 import BasicModalDialog from "../components/Caretaker/Modal";
+import LayersOutlinedIcon from "@mui/icons-material/LayersOutlined";
+import PlotModal from "../components/Caretaker/PlotModal";
 
 function CaretakerDashboard() {
-	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [modalState, setModalState] = useState({
+		basicModalOpen: false,
+		plotModalOpen: false,
+	});
 
-	const handleIconClick = () => {
-		setModalIsOpen(true);
-	};
-
-	const handleClose = () => {
-		setModalIsOpen(false);
+	const toggleModal = (modal) => {
+		setModalState((prevState) => ({
+			...prevState,
+			[modal]: !prevState[modal],
+		}));
 	};
 
 	return (
 		<>
 			<BasicModalDialog
-				modalIsOpen={modalIsOpen}
-				handleClose={handleClose}
+				modalIsOpen={modalState.basicModalOpen}
+				handleClose={() => toggleModal("basicModalOpen")}
+			/>
+			<PlotModal
+				modalIsOpen={modalState.plotModalOpen}
+				handleClose={() => toggleModal("plotModalOpen")}
 			/>
 			<div className="bg-blue-600 h-[450px] pt-12">
 				<Time />
@@ -29,14 +36,22 @@ function CaretakerDashboard() {
 				<div className="bg-white mx-2 rounded-t-3xl shadow-2xl h-full mt-4">
 					<div className="flex flex-row justify-between p-4 mx-2">
 						<p className="">Center Cemetery</p>
-						<p>
+						<div>
 							<AddCircleOutlineOutlinedIcon
 								fontSize="medium"
 								className="text-blue-600 hover:cursor-pointer"
-								onClick={handleIconClick}
+								onClick={() => toggleModal("basicModalOpen")}
 							/>
-						</p>
+							<LayersOutlinedIcon
+								fontSize="medium"
+								className="text-blue-600 hover:cursor-pointer ml-2"
+								onClick={() => toggleModal("plotModalOpen")}
+							/>
+						</div>
 					</div>
+					<p className="ml-6 -mt-2 text-xs">
+						<LayersOutlinedIcon fontSize="small" /> 4 Available Plots
+					</p>
 					<Kalag isAdmin={true} />
 				</div>
 			</div>
