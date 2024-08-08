359c1f822fe6fafeb2a306b36cceb0849964887b
project2479 <jaywrsnp6@gmail.com>
2024-08-08 19:21:49 +0800

    new face

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -4,37 +4,8 @@ import { motion } from "framer-motion";
 function Homepage() {
 	return (
 		<>
-			<div className="relative isolate overflow-hidden bg-gray-200 h-screen">
-				<svg
-					className="absolute inset-0 -z-10 h-full w-full stroke-white/40 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-					aria-hidden="true">
-					<defs>
-						<pattern
-							id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
-							width="200"
-							height="200"
-							x="100%"
-							y="-1"
-							patternUnits="userSpaceOnUse">
-							<path
-								d="M.5 200V.5H200"
-								fill="none"></path>
-						</pattern>
-					</defs>
-					<svg
-						x="50%"
-						y="-1"
-						className="overflow-visible fill-gray-800/20">
-						<path
-							d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
-							strokeWidth="0"></path>
-					</svg>
-					<rect
-						width="100%"
-						height="100%"
-						strokeWidth="0"
-						fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"></rect>
-				</svg>
+			<div className="relative isolate overflow-hidden bg-gray-200/70 h-screen">
+				
 
 				<div
 					className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
@@ -110,7 +81,7 @@ function Homepage() {
 								stiffness: 150,
 								damping: 25,
 							}}
-							className="mt-4 text-gray-400">
+							className="mt-4 text-gray-800">
 							<Link to={"/login"}>Caretaker Login</Link>
 						</motion.p>
 					</div>
